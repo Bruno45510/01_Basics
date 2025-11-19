dedd7a7687933dde7cbc3f1f24419759c71afe01
bruno <bruno@example.com>
2025-11-19 22:25:05 +0100

    41 If Aufgabe mit Else done

---
+++ 41_JS_If-Aufgaben_Else/aufgabe1.js
@@ -0,0 +1,17 @@
+let b = 0
+
+if (b >= 0) {
+    console.log("The number is positive");
+    
+}else console.log("The number is not positive");
+
+if (b <= 0) {
+    console.log( "The number is negative");
+    
+}else console.log("The number is positiv or 0");
+
+if (b == 0) {
+    console.log("The number is zero" );
+    
+}else console.log( "The number is not 0");
+
