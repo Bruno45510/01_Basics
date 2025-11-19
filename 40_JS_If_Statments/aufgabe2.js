4128b2f9ba4cf1c5691e48021eef8aed967b2c86
bruno <bruno@example.com>
2025-11-19 16:19:42 +0100

    40 If-Statements Aufgabe 3 done

---
+++ 40_JS_If_Statments/aufgabe2.js
@@ -0,0 +1,16 @@
+let trafficlight = "red"
+
+if (trafficlight == "green") {
+    console.log("You may go");
+    
+}
+
+if (trafficlight == "yellow") {
+    console.log("Caution!");
+    
+}
+
+if (trafficlight == "red") {
+    console.log("Stop!");
+    
+}
