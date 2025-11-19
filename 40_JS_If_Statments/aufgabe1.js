936280b5515c0148eecdd0c9456f27a1105bde51
bruno <bruno@example.com>
2025-11-19 16:12:04 +0100

    40 If-Statsments Aufgabe 1 done

---
+++ 40_JS_If_Statments/aufgabe1.js
@@ -0,0 +1,17 @@
+let a = 1
+
+if (a >= 0 ) {
+    console.log("the number is positive");
+    
+}
+
+if (a <= 0) {
+    console.log("the number is negative");
+    
+}
+
+if (a == 0) {
+    console.log("the number is zero");
+    
+}
+
