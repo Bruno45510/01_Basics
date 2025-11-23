bd69b2a5bafd64b03c259192421acdc9d83bbd76
bruno <bruno@example.com>
2025-11-24 00:04:59 +0100

    45 Deno Aufgabe 4

---
+++ 45_Deno/aufgabe4.js
@@ -0,0 +1,19 @@
+let alter = prompt("Gib mir dein alter")
+
+let alter1 = parseInt(alter)
+
+if (alter1 <= 10) {
+    console.log("You are young");
+    
+}
+
+if (alter1 == 10) {
+    console.log("You are middle old");
+    
+    
+}
+
+if (alter1 >= 20) {
+    console.log("You are old");
+    
+}
