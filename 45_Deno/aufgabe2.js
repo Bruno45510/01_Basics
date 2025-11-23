79841396554e3ece5a3e1f3c5b22ff57ae2b37c4
bruno <bruno@example.com>
2025-11-23 22:20:57 +0100

    45 Deno Aufgabe 2 done

---
+++ 45_Deno/aufgabe2.js
@@ -0,0 +1,18 @@
+let input = prompt("Gib mir eine Zahl")
+
+let input1 = parseInt(input)
+
+if (input1 >= 0) {
+    console.log("The number is positive");
+    
+}else console.log("The number is not positive");
+
+if (input1 <= 0) {
+    console.log("The number is negative");
+    
+}else console.log("The number is positive or 0");
+
+if (input1 == 0) {
+    console.log("The number is zero");
+    
+}else console.log("the number is not zero");
