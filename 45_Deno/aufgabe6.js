f14e6e5059a9b635490a717a2aeb627ef49ea1c2
bruno <bruno@example.com>
2025-11-24 00:30:10 +0100

    45 Deno Aufgabe 6 done

---
+++ 45_Deno/aufgabe6.js
@@ -0,0 +1,21 @@
+let drink = prompt("What drink do you want Cola=1, Water=2, Bier=3 or Tee=4")
+
+if (drink == 1) {
+    console.log("Thanks for your order of Cola");
+    
+}
+
+if (drink == 2) {
+    console.log("Thanks for your order of Water");
+    
+}
+
+if (drink == 3) {
+    console.log("Thanks for your order of Bier");
+    
+}
+
+if (drink == 4) {
+    console.log("Thanks for your order of Tee");
+    
+}
