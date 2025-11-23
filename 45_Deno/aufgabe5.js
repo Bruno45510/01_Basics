2a7fa16f1d334ab292d04ea947e115ba0eed0dac
bruno <bruno@example.com>
2025-11-24 00:19:54 +0100

    45 Deno Aufgabe 5

---
+++ 45_Deno/aufgabe5.js
@@ -0,0 +1,16 @@
+let weather = prompt("1=rainy 2=sunny 3=cold "+" "+"What's the weather like today?")
+
+if (weather == 1) {
+    console.log("Stay inside!");
+    
+}
+
+if (weather == 2) {
+    console.log("Go outside!");
+    
+}
+
+if (weather == 3) {
+    console.log("Dress warmly!");
+    
+}
