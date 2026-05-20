786073b8406c7432b1253f9e6f92b64017ccb756
bruno <bruno@example.com>
2026-05-20 09:34:33 +0200

    javascript übung rechner

---
+++ 01_Web/Web-Script/01.js
@@ -0,0 +1,18 @@
+document.getElementById("button").addEventListener("click", function () {
+    const element = document.getElementById("content");
+    element.innerHTML = "yessisrski";
+});
+
+document.getElementById("add").addEventListener("click", function () {
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+
+    if (isNaN(value1) || isNaN(value2)) {
+        alert("invalid numbers");
+    } else {
+        const result = parseFloat(value1) + parseFloat(value2);
+        document.getElementById("result").innerHTML = "Result: " + result;
+    }
+
+    console.log(value1);
+});
