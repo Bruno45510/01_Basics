0c726642c186e8fe517cb36207e00a873754fa59
bruno <bruno@example.com>
2026-06-02 08:45:30 +0200

    rechner + - * / gemacht

--- 01_Web/Web-Script/Rechner/01.js
+++ 01_Web/Web-Script/Rechner/01.js
@@ -16,3 +16,42 @@ document.getElementById("add").addEventListener("click", function () {
 
     console.log(value1);
 });
+document.getElementById("add1").addEventListener("click", function () {
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+
+    if (isNaN(value1) || isNaN(value2)) {
+        alert("invalid numbers");
+    } else {
+        const result = parseFloat(value1) - parseFloat(value2);
+        document.getElementById("result").innerHTML = "Result: " + result;
+    }
+
+    console.log(value1);
+});
+document.getElementById("add2").addEventListener("click", function () {
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+
+    if (isNaN(value1) || isNaN(value2)) {
+        alert("invalid numbers");
+    } else {
+        const result = parseFloat(value1) * parseFloat(value2);
+        document.getElementById("result").innerHTML = "Result: " + result;
+    }
+
+    console.log(value1);
+});
+document.getElementById("add3").addEventListener("click", function () {
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+
+    if (isNaN(value1) || isNaN(value2)) {
+        alert("invalid numbers");
+    } else {
+        const result = parseFloat(value1) / parseFloat(value2);
+        document.getElementById("result").innerHTML = "Result: " + result;
+    }
+
+    console.log(value1);
+});
